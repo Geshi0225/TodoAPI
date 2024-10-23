44ff79e9bc584cdf786e9bf3572aa4f2fa4961c2
riyousya <riyousya@DESKTOP-4PI3E2I>
2024-10-23 11:20:56 +0900

    意味が分からない箇所が多かったので、Copilot君に拝み倒してコメントを追加。
    TodoListをDBとしてサービスコンテナに認識させた？のかもしれない。

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -1,13 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using TodoAPI.Models;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+// Webアプリケーションのビルダーを作成して、コマンドライン引数を受け取る。
 
 builder.Services.AddControllers();
+// コントローラーをサービスコンテナに追加する。これでMVCのコントローラーを使えるようになる。
+
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+// DbContextをサービスコンテナに追加し、InMemoryDatabaseを使うように設定する。これでTodoContextがInMemoryDatabaseを使用するようになる。
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+// エンドポイントのAPIエクスプローラーをサービスコンテナに追加する。これでAPIエンドポイントが自動で探索されるようになる。
+
 builder.Services.AddSwaggerGen();
+// Swaggerの生成サービスを追加する。これでAPIドキュメントを自動生成できる。
 
 var app = builder.Build();
+// 構築したWebアプリケーションをビルドする。
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,11 +27,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+// 開発環境の場合、SwaggerとSwagger UIを使ってAPIドキュメントを提供する。
 
 app.UseHttpsRedirection();
+// HTTPリクエストをHTTPSにリダイレクトする。
 
 app.UseAuthorization();
+// 認可ミドルウェアを使用する。
 
 app.MapControllers();
+// コントローラーのルートマッピングを設定する。
 
 app.Run();
